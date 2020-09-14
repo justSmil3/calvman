d4ea78528d950b09ce36844798bbef266b730597
Christian Lott <christian.lott@protonmail.com>
2020-09-14 14:32:25 +0200

    anpassung der werte

--- Assets/RopePhysics/Prefabs/simpleDebugScript.cs
+++ Assets/RopePhysics/Prefabs/simpleDebugScript.cs
@@ -4,8 +4,5 @@ using UnityEngine;
 
 public class simpleDebugScript : MonoBehaviour
 {
-    private void OnCollisionStay(Collision collision)
-    {
-        Debug.LogError(this.name);
-    }
+    //depricated
 }
